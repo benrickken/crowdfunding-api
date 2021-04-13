b5e73407ae7a18aef6efaa9dad91e7f9dc03e35d
benrickken <benrickken@gmail.com>
2021-04-13 14:13:05 +0900

    Add API::V1::ProjectsController#create

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,6 +1,35 @@
 class Api::V1::ProjectsController < ApplicationController
+  before_action :set_auth, only: [:create]
+
   def index
     projects = Project.order(created_at: :desc)
     render json: { projects: projects }
+  end
+
+  def create
+    render json: @auth, status: :unauthorized and return unless @auth[:data]
+
+    uid = @auth[:data]['uid']
+    puts uid
+    user = User.find_by(uid: uid)
+    render json: { message: 'User does not exist' }, status: :bad_request and return if user.blank?    
+
+    project = user.projects.new(project_params)
+
+    if project.save
+      render json: { project: project }
+    else
+      render json: project.errors.messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+  def set_auth
+    @auth = authenticate_token_by_firebase
+  end
+
+  def project_params
+    params.require(:project).permit(:title, :target_amount, :due_date, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
-      resources :projects, only: [:index]
+      resources :projects, only: [:index, :create]
     end
   end
 end
