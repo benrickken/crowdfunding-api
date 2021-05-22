14cbac57585867f560ef58e15d232ea94b8f46a6
benrickken <benrickken@gmail.com>
2021-05-22 23:08:29 +0900

    Add test for Project model's instance methods

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -24,4 +24,39 @@ RSpec.describe Project, type: :model do
       expect(build(:project, due_date: nil)).to be_invalid
     end
   end
+
+  describe 'instance methods' do
+    let(:project) { build(:project) }
+
+    describe '#image_url' do
+      subject { project.image_url }
+
+      context 'when image is empty' do
+        it { is_expected.to eq(nil) }
+      end
+
+      context 'when image is not empty' do
+        before do
+          project.image = fixture_file_upload('spec/fixtures/example.svg')
+        end
+
+        it { is_expected.to match(%r{^http://localhost:3001/rails/active_storage/blobs/redirect/.*/example.svg}) }
+      end
+    end
+
+    describe '#supported_amount' do
+      before do
+        project_return1 = create(:project_return, project: project, price: 10_000)
+        project_return2 = create(:project_return, project: project, price: 20_000)
+        project_return3 = create(:project_return, project: project, price: 30_000)
+        create(:project_support, project_return: project_return1)
+        create(:project_support, project_return: project_return1)
+        create(:project_support, project_return: project_return2)
+        create(:project_support, project_return: project_return3)
+      end
+
+      subject { project.supported_amount }
+      it { is_expected.to eq(70_000) }
+    end
+  end
 end
