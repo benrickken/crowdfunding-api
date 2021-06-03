c421f67dfb213fc70423f1b34b86d40643068a42
benrickken <benrickken@gmail.com>
2021-06-03 10:46:33 +0900

    Add RSpec for ProjectSupport.create_with_complete_check!

--- spec/models/project_support_spec.rb
+++ spec/models/project_support_spec.rb
@@ -24,4 +24,17 @@ RSpec.describe ProjectSupport, type: :model do
       expect(build(:project_support, user_id: nil)).to be_invalid
     end
   end
+
+  describe 'class methods' do
+    describe '.create_with_complete_check!' do
+      let(:user) { create(:user) }
+      let(:project_return) { create(:project_return) }
+      let(:params) { { project_return_id: project_return.id } }
+      subject { ProjectSupport.create_with_complete_check!(user: user, params: params) }
+
+      it 'creates project_support' do
+        expect { subject }.to change(ProjectSupport, :count).by(1)
+      end
+    end
+  end
 end
