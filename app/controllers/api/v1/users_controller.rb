8063164a97aa7dfa2b4d1f095c175b4bd075a0b7
benrickken <benrickken@gmail.com>
2021-06-03 19:12:27 +0900

    Add UnauthenticatedError

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   def create
     auth = authenticate_token_by_firebase
-    render json: auth, status: :unauthorized and return unless auth[:data]
+    raise UnanthenticatedError unless auth[:data]
 
     uid = auth[:data]['uid']
     user = User.new(user_params.merge(uid: uid))
