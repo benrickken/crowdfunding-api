d811b708049e248f4e72d84806a363aa729ba629
benrickken <benrickken@gmail.com>
2021-06-05 13:18:20 +0900

    Add basic layout for admin page

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,6 @@
 class Admin::BaseController < ApplicationController
   protect_from_forgery with: :exception
   http_basic_authenticate_with name: ENV['ADMIN_BASIC_AUTH_NAME'], password: ENV['ADMIN_BASIC_AUTH_PASSWORD']
+
+  layout 'admin'
 end
