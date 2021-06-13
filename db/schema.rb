9e5669da7320b0ef06787dce4601ce1508473369
benrickken <benrickken@gmail.com>
2021-06-13 22:16:23 +0900

    Add email to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_13_123256) do
+ActiveRecord::Schema.define(version: 2021_06_13_125018) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2021_06_13_123256) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.string "email", null: false
     t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
