2cd588c8ed514f88f54efc9c46ba3f65cf26c2b9
benrickken <benrickken@gmail.com>
2021-06-15 09:13:49 +0900

    Add rake task to send batch emails

--- app/mailers/project_mailer.rb
+++ app/mailers/project_mailer.rb
@@ -2,6 +2,6 @@ class ProjectMailer < ApplicationMailer
   def completion_email
     @project = params[:project]
 
-    mail(to: 'benrickken@gmail.com', subject: 'プロジェクトが目標金額に到達しました。')
+    mail(to: @project.user.email, subject: 'プロジェクトが目標金額に到達しました。')
   end
 end
