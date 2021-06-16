36b09127f5e4aa2a08bbe7d1c7aa682af1d29ffa
benrickken <benrickken@gmail.com>
2021-06-16 10:25:57 +0900

    Add POST /project/:project_id/comments

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,12 +1,30 @@
 class Api::V1::CommentsController < Api::V1::BaseController
+  before_action :authenticate_user!, only: [:create]
+
   def index
-    project = Project.find(params[:id])
-    comments = project.comments
+    project = Project.find(params[:project_id])
+    comments = project.comments.includes(:user).order(created_at: :desc)
 
     render json: {
       comments: comments.map do |comment|
                   CommentSerializer.new(comment: comment).as_json
                 end
     }
+  end
+
+  def create
+    comment = current_user.comments.new(comment_params)
+
+    if comment.save
+      render json: { comment: comment }
+    else
+      render json: project.errors.messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body).merge(project_id: params[:project_id])
   end
 end
