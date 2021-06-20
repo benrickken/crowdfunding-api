3fe14a3a07e90cceafe1dd0a0d96e71eca63daf0
benrickken <benrickken@gmail.com>
2021-06-20 15:29:25 +0900

    Add error cases to .create_with_notification

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -19,15 +19,39 @@ RSpec.describe Comment, type: :model do
     describe '.create_with_notification' do
       let(:user) { create(:user) }
       let(:project) { create(:project) }
-      let(:params) { { body: 'My Comment' } }
       subject { Comment.create_with_notification(user: user, project: project, params: params) }
 
-      it 'creates project_support' do
-        expect { subject }.to change(Comment, :count).by(1)
+      context 'when params are valid' do
+        let(:params) { { body: 'My Comment' } }
+        it 'creates comment' do
+          expect { subject }.to change(Comment, :count).by(1)
+        end
+
+        it 'creates notification' do
+          expect { subject }.to change(Notification, :count).by(1)
+        end
+
+        it 'should not have error information' do
+          comment = subject
+          expect(comment.errors).to be_empty
+        end
       end
 
-      it 'creates notification' do
-        expect { subject }.to change(Notification, :count).by(1)
+      context 'when params are invalid' do
+        let(:params) { { body: '*' * 101 } }
+
+        it 'does not create comment' do
+          expect { subject }.not_to change(Comment, :count)
+        end
+
+        it 'does not create notification' do
+          expect { subject }.not_to change(Notification, :count)
+        end
+
+        it 'should have error information' do
+          comment = subject
+          expect(comment.errors).not_to be_empty
+        end
       end
     end
   end
